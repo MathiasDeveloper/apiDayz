12ccdf2f244827c619849fc95e86a14e1b4f23da
MathiasRbr <mathias.robert35@hotmail.fr>
2021-10-23 16:35:18 +0200

    Add jwt bundle

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\TokenParser\DumpTokenParser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,8 @@ class LoginController extends AbstractController
     /**
      * @Route("/auth/login", name="login")
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        return new JsonResponse('you called me');
+        return new JsonResponse("nom :" . $name . " and your password : " . $password);
     }
 }
